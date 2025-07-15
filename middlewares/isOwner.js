300d29485d2556ff91532918d4604e862ede7511
03pranay <ggpranayy@gmail.com>
2025-07-15 15:54:54 +0530

    initial commit

--- middlewares/isOwner.js
+++
@@ -1,9 +0,0 @@
-function isOwner(req, res, next) {
-  if (req.isAuthenticated() && req.user && req.user.role === 'owner') {
-    return next();
-  }
-  req.flash('error', 'You are not authorized to access this page.');
-  res.redirect('/login'); // Or a 403 page
-}
-
-module.exports = { isOwner };
